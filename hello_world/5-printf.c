29de3416e75f623c7bbec863066d4820b19089e2
roaa989 <roaahamoush98@gmail.com>
2024-02-19 14:48:48 +0000

    Hello puts

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    printf("with proper care, a dog is mans best friend,\n");
+    return (0);
+}
