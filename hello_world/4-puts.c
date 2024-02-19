718d80a1833dacc9668ade8c62158f2d631f2ed8
roaa989 <roaahamoush98@gmail.com>
2024-02-19 14:17:44 +0000

    Hello puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  // Print each word and space separately using puts
+  puts("Programming ");
+  puts("is ");
+  puts("like ");
+  puts("building ");
+  puts("a ");
+  puts("multilingual ");
+  puts("puzzle,");
+  puts("\n");  // Print a newline character
+
+  return 0;
+}
