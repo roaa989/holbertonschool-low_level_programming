80bec7564f6e60d392734e120835805006de1f42
roaa989 <roaahamoush98@gmail.com>
2024-02-19 18:55:26 +0000

    Hello puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
 /**
- * main -Entry point
- *
- * Return: Always 0
- */
-int main (void)
+ * main - A program that prints a line with puts function
+ * Return: 0 (success)
+*
+*/
+int main(void)
 {
-  	 puts("\"Programming is like building a multilingual puzzle");
-		return (O);
-}  
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
