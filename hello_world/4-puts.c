7c3c1965e0c78fcaea105bba6763f8000afdb111
roaa989 <roaahamoush98@gmail.com>
2024-02-19 15:00:49 +0000

    Hello puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main -Entry point
+ *
+ * Return: Always 0 (Succwss)
  */
-int main(void)
+int main (void)
 {
-    printf("with proper care, a dog is mans best friend,\n");
-    return (0);
-}
+  puts("\"Programming is like building a multilingual puzzle");
+  return (O);
+}  
