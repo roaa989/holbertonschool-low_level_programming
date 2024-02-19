2a56a6d7e284d9f7a1f9359a15355895165eea48
roaa989 <roaahamoush98@gmail.com>
2024-02-19 20:12:15 +0000

    Hello printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that a line using the printf function
+ * main - Entry pint
+ *
  * Return: 0 (success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
